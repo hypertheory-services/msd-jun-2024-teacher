c02729b1cd61c1b6e7511bba8182f2a8be725087
Jeff Gonzalez <jeff@hypertheory.com>
2024-06-04 16:28:11 -0400

    done day 2

---
+++ logical-boundaries/help-desk/IssueTrackerSolution/HelpDeskSharedMessages/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace HelpDeskSharedMessages;
+
+// What the IssueTracker API is going to send to the SupportApi
+public class HelpDeskIssueCreated
+{
+    public Guid Id { get; set; }
+    public Guid SoftwareId { get; set; }
+    public Guid UserId { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string SoftwareTitle { get; set; } = string.Empty;
+    public string UserEmailAddress { get; set; } = string.Empty;
+
+
+}
+
+// This is what the SupportApi is going to send back to us.
+
+public class TechAssignedToIssue
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
